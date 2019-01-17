29a20d402667814e1661a247b57cc82624fbe286
Keshav Agarwal <keshavag007@gmail.com>
2019-01-17 11:56:35 +0530

    My first Commit

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Calculator{
+	public static void main(String args[]){
+		int a = 10;
+		int b =5;
+		System.out.println("Addition of these two numbers "+ (a+b));
+	}
+}
